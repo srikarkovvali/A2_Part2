bae8c1f016a7a250c2fe00fcf636a3199e2ba600
Srikar Kovvali <srikarkovvali@Srikars-MacBook-Pro.local>
2020-10-13 21:53:57 -0400

    Fresh Commit

--- server.py
+++ server.py
@@ -1,108 +1,21 @@
-#arbab code - need to change
+import client
+from client_split_messages import *
+import sys
+import utils
 
-import socket
-import thread
+sockets = [] #we use a list/array for the sockets
+channels = {} #but we use a data dictionary for the channels because we need added functionality from them
+sock_buff = 2048
+sock_names = {} #names of all the sockets
+partial_mess = ""
 
+def chatserver():
+    port = int(sys.argv)  #get server port from the user
+    host_ip = ""
+    socket_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket creation
+    socket_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_s.bind((host_ip, port))
+    socket_s.listen(10)
 
-class Client(object):
-
-    def __init__(self, name, address, socket):
-        self.name = name
-        self.address = address
-        self.socket = socket
-
-
-class Channel(object):
-
-    def __init__(self, name):
-        self.name = name
-        self.subscriber = []
-
-
-serverName = 'localhost'
-serverPort = 12000
-clients = []
-channels = []
-
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind((serverName, serverPort))
-server_socket.listen(5)
-
-
-def checkOperation(message):
-    if (message[0] == '/'):
-        return True
-    else:
-        return False
-
-
-def parseOperation(message):
-    print message[1:5]
-    if (message[1:5] == 'list'):
-        return 'list'
-    elif (message[1:5] == 'join'):
-        return 'join'
-    elif (message[1:7] == 'create'):
-        return 'create'
-
-
-def parseChannelName(message):
-    Channel = message.split()[1]
-    return Channel
-
-
-def returnChannelList(socket):
-    channelMessage = ''
-    for channel in channels:
-        channelMessage += channel.name + '\n'
-    print channelMessage
-    socket.send(channelMessage)
-
-
-def createChannel(socket, channelName):
-    newChannel = Channel(channelName)
-    channels.append(newChannel)
-    print channels
-
-
-def joinChannel(socket, channelName, clientName):
-    channelObj
-    for channel in channels:
-        if channel.name == channelName:
-            channelObj = channel
-    channel.subscriber.append(clientName)
-
-
-def listenClient(client):
-    while True:
-        message = client.socket.recv(1024)
-        print("CLient: " + client.name)
-        print("Message: " + message)
-        if checkOperation(message):
-            operation = parseOperation(message)
-            if (operation == 'list'):
-                returnChannelList(client.socket)
-            elif (operation == 'create'):
-                createChannel(client.socket, parseChannelName(message))
-            elif (operation == 'join'):
-                joinChannel(client.socket, parseChannelName(message), client.name)
-        else:
-            print("Message: " + message)
-
-
-while True:
-    connectionSocket, addr = server_socket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    clientInfo = sentence.split()
-    client = Client(clientInfo[0], addr, connectionSocket)
-    clients.append(client)
-    print('new client')
-    thread.start_new_thread(listenClient, (client,))
-
-
-
-
-
-
-
-
+    sockets.append(socket_s) #append the socket to connections
+    while not False:
